4ac8bbab6870a9051d1594da051176afe6163c59
Noozq <142098533+Noozq@users.noreply.github.com>
2024-02-02 23:44:20 +0100

    main.py aktualisieren

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord 
+from discord.ext import discord
+import os
+import json
+
+with open('setting/data.json') as f:
+
+intents = 
+client = commands.Bot(command_prefix = get_prefix, intents=intents)
+
+client.run(token)
