d52a5074e349e3eddae5950f32d51948997a172b
Noozq <142098533+Noozq@users.noreply.github.com>
2024-02-03 14:54:21 +0000

    main

--- main.py
+++ main.py
@@ -2,8 +2,73 @@ import discord
 from discord.ext import commands
 import os
 import json
+import colorama
+from colorama import Fore
+import dotenv
+from dotenv import load_dotenv
+
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
+version = str(os.getenv("VERSION"))
+
+
+def get_prefix(client, message):
+    with open("config/prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = get_prefix, intents=intents)
+@client.event
+async def on_ready():
+  print(Fore.RED + 
+  '''
+  ██╗   ██╗ █████╗ ██╗   ██╗███████╗██╗ ██████╗ 
+  ██║   ██║██╔══██╗╚██╗ ██╔╝╚══███╔╝██║██╔═══██╗
+  ██║   ██║███████║ ╚████╔╝   ███╔╝ ██║██║   ██║
+  ╚██╗ ██╔╝██╔══██║  ╚██╔╝   ███╔╝  ██║██║   ██║
+   ╚████╔╝ ██║  ██║   ██║   ███████╗██║╚██████╔╝
+    ╚═══╝  ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝ ╚═════╝ 
+                                              '''
+  )
+
+@client.event
+async def on_guild_join(guild):
+  with open('config/prefixes.json', 'r') as f:
+    prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = 'v!'
+
+  with open('config/prefixes.json', 'w') as f:
+    json.dump(prefixes, f, indent=4)
+
+  with open('config/data/guildinfos.json', 'r') as f:
+      data = json.load(f)
+
+      data["Guildid"] = str(guild.id)
+      data["Guildname"] = str(guild.name)
+
+  with open('config/data/guildinfos.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+
+@client.event
+async def on_guild_remove(guild):
+  with open('config/prefixes.json', 'r') as f:
+    prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('config/prefixes.json', 'w') as f:
+      json.dump(prefixes, f, indent=4)
+
+  with open('config/data/guildinfos.json', 'r') as f:
+      data = json.load(f)
+
+      data["Guildid"].pop(str(guild.id))
+      data["Guildname"].pop(str(guild.name))
+
+/
 
 client.run(token)
