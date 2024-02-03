08db54b6c8e16a881b79f87d12081ca7649e1a55
Noozq <142098533+Noozq@users.noreply.github.com>
2024-02-03 14:04:33 +0100

    main.py aktualisieren

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import discord 
-from discord.ext import discord
+from discord.ext import commands
 import os
 import json
 
-with open('setting/data.json') as f:
-
-intents = 
+intents = discord.Intents.all()
 client = commands.Bot(command_prefix = get_prefix, intents=intents)
 
 client.run(token)
